c23ec40cbc0df7c8847d403ac3bde6c51ad1b4e5
echigah <capurissedgard@gmail.com>
2021-10-27 19:42:49 -0300

    push

--- src/modules/tramite/application/obtenerTramitePorId.js
+++ src/modules/tramite/application/obtenerTramitePorId.js
@@ -2,19 +2,13 @@ function crearObtenerTramitePorId(tramite, licenciaconducirdatos) {
     return {
         async ejecutar(id) {
             if (!id) { throw new Error("tramite invalido o faltante") }
-           
-            await tramite.obtenerDatos(id)           
-
-            if (!tramite.idProcedureType) {throw new Error("No se pudo identificar el tipo de tramite.")}
-
-            
-
+            await tramite.obtenerDatos(id)
+            if (!tramite.idProcedureType) { throw new Error("No se pudo identificar el tipo de tramite.") }
             switch (tramite.idProcedureType) {
-                case 1:                   
-                    await licenciaconducirdatos.obtenerDatos(tramite.id)                        
+                case 1:
+                    await licenciaconducirdatos.obtenerDatos(tramite.id)
                     break;
-            }            
-
+            }
             return licenciaconducirdatos
         }
     }
