e7a36bb3044b28361f7fa771cd7134167ea40b2a
echigah <capurissedgard@gmail.com>
2021-10-30 10:19:54 -0300

    step 1 notifications

--- src/modules/usuario/application/obtenerUsuariosFactory.js
+++
@@ -1,9 +0,0 @@
-import getUsuario from "../model/usuarioFactory.js";
-import crearObtenerUsuario from "./obtenerUsuarios.js"
-
-let usuario = getUsuario()
-let obtenerUsuario = crearObtenerUsuario(usuario)
-
-function getObtenerUsuarios() {return obtenerUsuario}
-
-export default getObtenerUsuarios
